ef8fb8a918380f8e318fec3dee6cdba134ac9630
Christopher Duarte <christopher.duarte@middletownct.gov>
2026-04-29 14:37:02 -0400

    Terms of Service

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -6,11 +6,38 @@ namespace Application\Controller;
 
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
+use Michelf\Markdown;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
         return new ViewModel();
+    }
+    
+    public function termsAction()
+    {
+        $view = new ViewModel();
+        
+        $parser = new Markdown();
+        $contents = file_get_contents(__DIR__ . '/../../../../TERMS.md');
+        $html = $parser->defaultTransform($contents);
+        
+        $view->setVariable('html', $html);
+        
+        return($view);
+    }
+    
+    public function privacyAction()
+    {
+        $view = new ViewModel();
+        
+        $parser = new Markdown();
+        $contents = file_get_contents(__DIR__ . '/../../../../PRIVACY.md');
+        $html = $parser->defaultTransform($contents);
+        
+        $view->setVariable('html', $html);
+        
+        return($view);
     }
 }
